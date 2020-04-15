40bb157b0dcd1a6f86bf4c066fff22992333bcf3
Prasad <airportprasad@gmail.com>
2020-04-15 14:35:16 +0530

    link to online git base support

---
+++ src/main/java/com/avsec/awms/AwmsApplication.java
@@ -0,0 +1,13 @@
+package com.avsec.awms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avsec/awms/AwmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avsec.awms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
